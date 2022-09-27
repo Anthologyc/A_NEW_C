908c6bb6793b49ad23348bd2bff4d081c9305c23
BuildTools <unconfigured@null.spigotmc.org>
2022-09-27 20:55:48 +0800

    NEW MESS

--- funcation/main.c
+++ funcation/main.c
@@ -2,6 +2,7 @@
 
 void fun_1();
 void fun_2();
+void fun_3();
 
 int main(void) {
 
@@ -19,7 +20,11 @@ int main(void) {
     case 2:
         fun_2();
         break;
-    
+
+    case 3:
+        fun_3();
+        break;
+
     default:
         printf("GG");
         break;
@@ -82,4 +87,29 @@ void fun_2() {
     }
 
     printf("bonus = %lf", bonus);
+}
+
+void fun_3() {
+
+    int  i, j, m, n, x;
+
+    for (i = 1; i < 168 / 2 + 1; i++)
+    {
+
+        if (168 % i == 0)
+        {
+
+            j = 168 / i;
+            if ( i > j && (i + j) % 2 == 0 && (i - j) % 2 == 0)
+            {
+
+                m = (i + j) / 2;
+                n = (i - j) / 2;
+                x = n * n - 100;
+
+                printf ("%d + 100 = %d * %d\n", x, n, n);
+                printf ("%d + 268 = %d * %d\n", x, m, m);
+            }
+        }
+    }
 }
