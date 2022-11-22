9c5b9a0c72b9e80d1666846f4a05e1efcce85c43
Anthologyc <anthologyc@foxmail.com>
2022-11-22 19:18:09 +0800

    NEW

--- homework/test.c
+++ homework/test.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#define SQUARES 3
 
-int main(void)
+int main (void)
 {
-    char ch = 85;
-    ch = 127;
-    printf("%d", ch);
+    const double CROP = 2e16;
+    double current, total;
+    int count = 1;
+
+    total = current = 1.0;
+
+    printf("%4d %13.2e %12.2e %12.2e\n", count, current, total, total / CROP);
+
+    while (count < SQUARES)
+    {
+        count = count + 1;
+        current = 2.0 * current;
+        total = total + current;
+        printf("%4d %13.2e %12.2e %12.2e\n", count, current, total, total / CROP);
+    }
+
+    printf("That's all.\n");
+
     return 0;
 }
